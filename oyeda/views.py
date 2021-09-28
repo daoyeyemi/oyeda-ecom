35746925975071a47b75f6abee621dd5a3f4d7c6
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-09-28 10:42:40 -0700

    added forms and new user registration

--- oyeda/views.py
+++ oyeda/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Shoe
 from django.views.generic import DetailView
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import SignupForm
 
 def home(request):
     context = {
@@ -15,10 +18,27 @@ def products(request):
     return render(request, 'products.html', context)
 
 def login(request):
-    return render(request, 'login.html')
+    context = {
+        # 'form' : form
+    }
+    return render(request, 'login.html', context)
 
 def signup(request):
-    return render(request, 'signup.html')
+    form = SignupForm()
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        # forms are primarily used to validate data so the is.valid() function returns 
+        # boolean stating whether or not value is true
+        if form.is_valid(): 
+            # .save() creates and saves object to database; if object already exists, it
+            # updates it instead
+            form.save()
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'signup.html', context)
 
 class ShoeDetailView(DetailView):
     model = Shoe
