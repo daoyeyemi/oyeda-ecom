03528b93ff3fbaaee97f52b223ee2785c858535b
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-09-28 17:26:24 -0700

    got signup function to work on views.py

--- oyeda/views.py
+++ oyeda/views.py
@@ -4,6 +4,8 @@ from .models import Shoe
 from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUser
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout 
 
 def home(request):
     context = {
@@ -17,12 +19,6 @@ def products(request):
     }
     return render(request, 'products.html', context)
 
-def login(request):
-    context = {
-        # 'form' : form
-    }
-    return render(request, 'login.html', context)
-
 def signup(request):
     form = CreateUser()
 
@@ -34,11 +30,45 @@ def signup(request):
             # .save() creates and saves object to database; if object already exists, it
             # updates it instead
             form.save()
-
+            # take saved information from form and withdraw 
+            user = form.cleaned_data.get('username')
+            
+            print(user)
+            
+            messages.success(request, 'User profile for ' + user + ' was successfully created')
+            
+            return redirect('oyeda:login')
+    
     context = {
         'form' : form
     }
+    
     return render(request, 'signup.html', context)
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        # authenticate() check credentials it gets and returns 
+        # object that matches the credentials if credentials are valid
+        # if they're not valid, it should return None
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # login takes in request and user object
+            # saves the user ID in the session, using django's
+            # session framework
+            login(request, user)
+            redirect('oyeda:login')
+
+    context = {
+
+    }
+
+    return render(request, 'login.html', context)
+
 
 class ShoeDetailView(DetailView):
     model = Shoe
