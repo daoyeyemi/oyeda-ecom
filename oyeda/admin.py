851a8df6b29079a5fbb9aed460aa90b977e5df0a
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-09-30 13:32:39 -0700

    added more models to project

--- oyeda/admin.py
+++ oyeda/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Shoe
+from .models import Shoe, OrderedItem, OrderList
 
 admin.site.register(Shoe)
+admin.site.register(OrderedItem)
+admin.site.register(OrderList)
--- oyeda/models.py
+++ oyeda/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 class Shoe(models.Model):
     name = models.CharField(max_length=100)
@@ -16,3 +17,19 @@ class Shoe(models.Model):
         return reverse('oyeda:individual-product', kwargs={
             'slug': self.slug
         })
+
+class OrderedItem(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    item = models.ForeignKey(Shoe, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"Item: {self.item.name} - Quantity: {self.quantity}"
+
+class OrderList(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderedItem)
+
+    def __str__(self):
+        return self.user.username
