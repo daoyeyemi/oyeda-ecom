d2b8396fac2319664561c25fb627582a85977026
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-10-21 18:22:17 -0700

    added some required fields

--- oyeda/forms.py
+++ oyeda/forms.py
@@ -15,14 +15,10 @@ class CreateUser(UserCreationForm):
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField()
+    street_address = forms.CharField(required=True)
     street_address_2 = forms.CharField(required=False)
-    city = forms.CharField()
-    zip = forms.IntegerField()
+    city = forms.CharField(required=True)
+    zip = forms.IntegerField(required=True)
     country = CountryField(blank_label='(Select country)').formfield(
         widget=CountrySelectWidget(attrs={ 'class' : 'country-select' }))
     payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
-
-
-    
-
